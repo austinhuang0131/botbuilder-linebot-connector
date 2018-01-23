df49b29c2e2342fef880a6e6b013fdcf058eb65b
Austin Huang <austinhuang0131@gmail.com>
2018-01-22 19:32:25 -0500

    Update LineConnector.js

--- line/LineConnector.js
+++ line/LineConnector.js
@@ -591,9 +591,12 @@ var LineConnector = /** @class */ (function () {
                         /*if (!be_same) {
                             throw new Error("must be same attachment");
                         }*/
+                        console.log(event);
                         if (event.attachments[0].contentType === "application/vnd.microsoft.card.hero") {
-                            var be_image_carousel = event.attachments.reduce(function (c, n) {
-                                return c.content.images && n.content.images && c.content.images.length === 1 && n.content.images.length === 1 && c.content.buttons.length === 1 && n.content.buttons.length === 1;
+                            var be_image_carousel = event.attachments.filter(e => {
+                                return c.content.images !== undefined && n.content.images !== undefined;
+                            }).reduce(function (c, n) {
+                                return c.content.images.length === 1 && n.content.images.length === 1 && c.content.buttons.length === 1 && n.content.buttons.length === 1;
                             });
                             if (be_image_carousel) {
                                 return {
